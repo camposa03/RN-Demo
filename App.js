a0b987a597a9ecd6eaecd9dda39655ee05505a53
Armando Campos <camposa03@yahoo.com>
2021-06-21 12:16:56 -0600

    Adding react navigation for routing functionality

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-    </View>
+    <NavigationContainer>
+       <View style={styles.container}>
+         <Text>Hello, world</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
